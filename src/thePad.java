b3951fc03daeac1ff7ca34f2741e99f03ac1f7ac
hotsauce86 <timothy_tierney@my.uri.edu>
2018-10-19 17:29:47 -0400

    Island
    added some method that create land similar to dwarf fortress

--- src/thePad.java
+++ src/thePad.java
@@ -1,3 +1,7 @@
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+
 public class thePad {
 
 
@@ -5,15 +9,22 @@ public class thePad {
 
         String countSpaceExample = "asdhfwer t asdljfno as doahedjkhnd ";
 
-
         System.out.println(countSpace(countSpaceExample));
-
+    ////////////////////////////////////////
         String s1 = "asbcsdsdf";
         String s2 = "sdfbeljkdcfj";
 
         MatchTwoString(s1,s2);
         stringMap(s1,s2);
+//////////////////////////////////////////////////
+
+       intMap(27);
+
+       islandMaker();
+
     }
+
+
 
     public static String countSpace(String x){
 
@@ -64,6 +75,147 @@ public class thePad {
         System.out.println(s1.replaceFirst(" ", s2));
         System.out.println(s1.toUpperCase());
         System.out.println(s1.matches("a"));
+        System.out.println(s1.replace("a", "A"));
+        System.out.println(s1.split("a"));
+        System.out.println(s1.replace("a", "T"));
+    }
+
+    public static void intMap (int x){
+        System.out.println(x);
+
+        int i1 = 27;
+        List<Integer> integerList = new LinkedList<>();
+        for(int i=0; i <10; i++){
+            //make random an int and output froma a range of 40
+            integerList.add((int)(Math.random()*40));
+            System.out.println(integerList.get(i));
+        }
+
+    }
+
+    public static void islandMaker(){
+        int[][] grid = new int[5][5];
+
+        //y axis
+        for(int i =0; i<grid.length; i++){
+            //x axis
+            for(int j=0; j< grid.length; j++){
+                grid[j][i] = 0;
+                System.out.print(" "+grid[j][i]);
+            }
+            System.out.println("");
+        }
+
+        System.out.println("");System.out.println("");
+        seedIsland(grid);
+
+        printOcean(grid);
+        System.out.println("");System.out.println("");
+
+        growIsland(grid);
+        printOcean(grid);
+        System.out.println("");System.out.println("");
+
+        growIsland(grid);
+        printOcean(grid);
+
+
+
+    }
+
+    public static int[][] seedIsland(int[][] someOcean){
+
+        for(int i =0; i<someOcean.length; i++){
+            //x axis
+            for(int j=0; j< someOcean.length; j++){
+                int x = (int)(Math.random()*50);
+
+                if(x==4){
+                    someOcean[j][i] = 1;
+                }
+            }
+        }
+        return someOcean;
+    }
+
+    public static void printOcean(int[][] someOcean){
+        for(int i =0; i<someOcean.length; i++){
+            //x axis
+            for(int j=0; j< someOcean.length; j++){
+                System.out.print(" "+someOcean[j][i]);
+            }
+            System.out.println("");
+        }
+    }
+
+    public static int[][] growIsland(int[][] someOcean){
+        //first we find the highest points on the map
+        int highestPoint =0;
+        for(int i =0; i<someOcean.length; i++){
+            for(int j=0; j< someOcean.length; j++){
+
+                if(someOcean[j][i]>highestPoint){
+                    highestPoint=someOcean[j][i];
+                }
+            }
+        }
+
+        //next we raise the highest points
+        for(int i =0; i<someOcean.length; i++){
+            for(int j=0; j< someOcean.length; j++){
+                if(someOcean[j][i]==highestPoint){
+                    someOcean[j][i]++;
+                }
+            }
+        }
+        highestPoint++;
+
+        //and now we raise the points around them
+
+        //well use secondWave as a way to add some randomness
+
+        //for each layer in the z axis
+        for(int k=0; k<highestPoint;k++) {
+            //the x axis
+            for (int i = 0; i < someOcean.length; i++) {
+                //the y axis
+                for (int j = 0; j < someOcean.length; j++) {
+
+                    //finding the hight at this point
+                    if (someOcean[j][i] == highestPoint-k) {
+                        //we check the immediate cardinal directions
+                        if ((j > 0 && j < someOcean.length - 1) && (i > 0 && i < someOcean.length - 1)) {
+                            int secondWave = (int)(Math.random()*2);
+                            if(secondWave==1) {
+                                //North
+                                if (someOcean[j][i - 1] < someOcean[j][i]) {
+                                    someOcean[j][i - 1]++;
+                                }
+                                //South
+                                if (someOcean[j][i + 1] < someOcean[j][i]) {
+                                    someOcean[j][i + 1]++;
+                                }
+                                //East
+                                if (someOcean[j + 1][i] < someOcean[j][i]) {
+                                    someOcean[j + 1][i]++;
+                                }
+                                //West
+                                if (someOcean[j - 1][i] < someOcean[j][i]) {
+                                    someOcean[j - 1][i]++;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+
+
+
+
+
+        return someOcean;
     }
 
 
